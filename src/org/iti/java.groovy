97af705658cbf108c7b90ab0990675d8dbbbb79a
Manar Hamada Elsayed <74185394+Manar20575@users.noreply.github.com>
2025-05-26 20:14:47 +0300

    Task2

---
+++ src/org/iti/java.groovy
@@ -0,0 +1,8 @@
+// package org.iti;
+
+// def build(){
+//     sh "mvn clean package install"
+// }
+// def test(){
+//     sh "mvn test"
+// }
---
+++ vars/pipelinePy.groovy
@@ -0,0 +1,67 @@
+def call(Map config = [:]) {
+    def docker = load "src/org/iti/Docker.groovy"
+    def dockerT1 = docker.new(this)
+    
+    node('java') {
+        stage('Checkout') {
+            checkout scm
+        }
+        
+        withCredentials([
+            usernamePassword(
+                credentialsId: 'dockerhub-user',
+                usernameVariable: 'DOCKER_USER',
+                passwordVariable: 'DOCKER_PASS'
+            )
+        ]) {
+            stage("Build Docker Image") {
+                dockerT1.build("${DOCKER_USER}/python", "${BUILD_NUMBER}")
+            }
+            
+            stage("Push Docker Image") {
+                dockerT1.login("${DOCKER_USER}", "${DOCKER_PASS}")
+                dockerT1.push("${DOCKER_USER}/python", "${BUILD_NUMBER}")
+            }
+        }
+    }
+}
+// def call(Map config = [:]) {
+//     def dockerT1 = new org.iti.Docker(this)
+//     pipeline {
+//         agent {
+//             label "java"
+//         }
+        
+//         environment {
+//             DOCKER_USER = credentials('dockerhub-user')
+//             DOCKER_PASS = credentials('docker-pass')
+//         }
+        
+//         parameters {
+//             string(
+//                 defaultValue: "${BUILD_NUMBER}", 
+//                 description: 'Current Image Version', 
+//                 name: 'VERSION'
+//             )
+//         }
+        
+//         stages {
+//             stage("Build Docker Image") {
+//                 steps {
+//                     script {
+//                         dockerT1.build("${DOCKER_USER}/python", "${params.VERSION}")
+//                     }
+//                 }
+//             }
+            
+//             stage("Push Docker Image") {
+//                 steps {
+//                     script {
+//                         dockerT1.login("${DOCKER_USER}", "${DOCKER_PASS}")
+//                         dockerT1.push("${DOCKER_USER}/python", "${params.VERSION}")
+//                     }            
+//                 }
+//             }
+//         }
+//     }
+// }
