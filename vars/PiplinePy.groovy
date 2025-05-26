4a177e3ef65cef13f074106d993a7e966c58bda1
Manar Hamada Elsayed <74185394+Manar20575@users.noreply.github.com>
2025-05-26 18:50:39 +0300

    Task2

--- vars/PiplinePy.groovy
+++ vars/PiplinePy.groovy
@@ -1,41 +1,64 @@
 #!/usr/bin/env groovy
 def call(Map config = [:]) {
     def dockerT1 = new org.iti.Docker(this)
-    pipeline {
-        agent {
-            label "java"
-        }
-        
-        environment {
-            DOCKER_USER = credentials('dockerhub-user')
-            DOCKER_PASS = credentials('docker-pass')
-        }
-        
-        parameters {
-            string(
-                defaultValue: "${BUILD_NUMBER}", 
-                description: 'Current Image Version', 
-                name: 'VERSION'
+    
+    node('java') {
+        withCredentials([
+            usernamePassword(
+                credentialsId: 'dockerhub-user', 
+                usernameVariable: 'DOCKER_USER', 
+                passwordVariable: 'DOCKER_PASS'
             )
-        }
-        
-        stages {
+        ]) {
             stage("Build Docker Image") {
-                steps {
-                    script {
-                        dockerT1.build("${DOCKER_USER}/python", "${params.VERSION}")
-                    }
-                }
+                dockerT1.build("${DOCKER_USER}/python", "${BUILD_NUMBER}")
             }
             
             stage("Push Docker Image") {
-                steps {
-                    script {
-                        dockerT1.login("${DOCKER_USER}", "${DOCKER_PASS}")
-                        dockerT1.push("${DOCKER_USER}/python", "${params.VERSION}")
-                    }            
-                }
+                dockerT1.login("${DOCKER_USER}", "${DOCKER_PASS}")
+                dockerT1.push("${DOCKER_USER}/python", "${BUILD_NUMBER}")
             }
         }
     }
 }
+
+// def call(Map config = [:]) {
+//     def dockerT1 = new org.iti.Docker(this)
+//     pipeline {
+//         agent {
+//             label "java"
+//         }
+        
+//         environment {
+//             DOCKER_USER = credentials('dockerhub-user')
+//             DOCKER_PASS = credentials('docker-pass')
+//         }
+        
+//         parameters {
+//             string(
+//                 defaultValue: "${BUILD_NUMBER}", 
+//                 description: 'Current Image Version', 
+//                 name: 'VERSION'
+//             )
+//         }
+        
+//         stages {
+//             stage("Build Docker Image") {
+//                 steps {
+//                     script {
+//                         dockerT1.build("${DOCKER_USER}/python", "${params.VERSION}")
+//                     }
+//                 }
+//             }
+            
+//             stage("Push Docker Image") {
+//                 steps {
+//                     script {
+//                         dockerT1.login("${DOCKER_USER}", "${DOCKER_PASS}")
+//                         dockerT1.push("${DOCKER_USER}/python", "${params.VERSION}")
+//                     }            
+//                 }
+//             }
+//         }
+//     }
+// }
