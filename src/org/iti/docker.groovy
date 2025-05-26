fea8571e79805229d0a2a968d898bbf67c497b0f
Manar Hamada Elsayed <74185394+Manar20575@users.noreply.github.com>
2025-05-26 18:37:32 +0300

    Task2

--- src/org/iti/docker.groovy
+++ src/org/iti/docker.groovy
@@ -7,15 +7,30 @@ class Docker implements Serializable {
         this.script = script
     }
 
-    def build(String imageName, String version) {
-        script.sh "docker build -t ${imageName}:${version} ."
+    def login(USERNAME, PASSWORD){
+        script.sh "docker login -u ${USERNAME} -p ${PASSWORD}"
     }
 
-    def login(String username, String password) {
-        script.sh "echo ${password} | docker login -u ${username} --password-stdin"
+    def build(IMAGE_NAME, IMAGE_TAG){
+        script.sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
     }
 
-    def push(String imageName, String version) {
-        script.sh "docker push ${imageName}:${version}"
+    def push(IMAGE_NAME, IMAGE_TAG){
+        script.sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
     }
 }
+
+
+// package org.iti;
+
+// def login(USERNAME, PASSWORD){
+//     sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+// }
+
+// def build(IMAGE_NAME, IMAGE_TAG){
+//     sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
+// }
+
+// def push(IMAGE_NAME, IMAGE_TAG){
+//     sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
+// }
--- vars/PiplinePy.groovy
+++ vars/PiplinePy.groovy
@@ -1,5 +1,5 @@
-#!/usr/bin/env groovy
 def call(Map config = [:]) {
+    def dockerT1 = new org.iti.Docker(this)
     pipeline {
         agent {
             label "java"
@@ -22,7 +22,6 @@ def call(Map config = [:]) {
             stage("Build Docker Image") {
                 steps {
                     script {
-                        def dockerT1 = new org.iti.Docker(this)
                         dockerT1.build("${DOCKER_USER}/python", "${params.VERSION}")
                     }
                 }
@@ -31,7 +30,6 @@ def call(Map config = [:]) {
             stage("Push Docker Image") {
                 steps {
                     script {
-                        def dockerT1 = new org.iti.Docker(this)
                         dockerT1.login("${DOCKER_USER}", "${DOCKER_PASS}")
                         dockerT1.push("${DOCKER_USER}/python", "${params.VERSION}")
                     }            
