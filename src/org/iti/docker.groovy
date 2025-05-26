8fb0962279243fe82b36c9947ceebc53ecf564d4
Manar Hamada Elsayed <74185394+Manar20575@users.noreply.github.com>
2025-05-26 18:22:33 +0300

    Task2

--- src/org/iti/docker.groovy
+++ src/org/iti/docker.groovy
@@ -1,15 +1,21 @@
-package org.iti;
-class docker {
+package org.iti
 
-def login(USERNAME, PASSWORD){
-    sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-}
+class Docker implements Serializable {
+    def script
 
-def build(IMAGE_NAME, IMAGE_TAG){
-    sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
-}
+    Docker(script) {
+        this.script = script
+    }
 
-def push(IMAGE_NAME, IMAGE_TAG){
-    sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
-}
+    def build(String imageName, String version) {
+        script.sh "docker build -t ${imageName}:${version} ."
+    }
+
+    def login(String username, String password) {
+        script.sh "echo ${password} | docker login -u ${username} --password-stdin"
+    }
+
+    def push(String imageName, String version) {
+        script.sh "docker push ${imageName}:${version}"
+    }
 }
--- vars/PiplinePy.groovy
+++ vars/PiplinePy.groovy
@@ -1,46 +1,42 @@
-def call(){
-    def dockerT1 = new org.iti.docker()
-
-pipeline{
-    agent{
-        label "java"
-    }
-    environment{
-        DOCKER_USER = credentials('dockerhub-user')
-        DOCKER_PASS = credentials('docker-pass')
-    }
+#!/usr/bin/env groovy
+def call(Map config = [:]) {
+    pipeline {
+        agent {
+            label "java"
+        }
+        
+        environment {
+            DOCKER_USER = credentials('dockerhub-user')
+            DOCKER_PASS = credentials('docker-pass')
+        }
+        
         parameters {
-        string defaultValue: '${BUILD_NUMBER}', description: 'Current Image Version', name: 'VERSION'
-    }
-    stages{
-        stage("build Docker image"){
-            steps{
-                script{
-                dockerT1.build("${DOCKER_USER}/python", "${VERSION}")
+            string(
+                defaultValue: "${BUILD_NUMBER}", 
+                description: 'Current Image Version', 
+                name: 'VERSION'
+            )
+        }
+        
+        stages {
+            stage("Build Docker Image") {
+                steps {
+                    script {
+                        def dockerT1 = new org.iti.Docker(this)
+                        dockerT1.build("${DOCKER_USER}/python", "${params.VERSION}")
+                    }
+                }
+            }
+            
+            stage("Push Docker Image") {
+                steps {
+                    script {
+                        def dockerT1 = new org.iti.Docker(this)
+                        dockerT1.login("${DOCKER_USER}", "${DOCKER_PASS}")
+                        dockerT1.push("${DOCKER_USER}/python", "${params.VERSION}")
+                    }            
                 }
             }
         }
-        stage("Push Docker image"){
-            steps{
-                script{
-                    dockerT1.login("${DOCKER_USER}", "${DOCKER_PASS}")
-                    dockerT1.push("${DOCKER_USER}/python", "${VERSION}")
-                }            
-            }
-        }
     }
-}
-
-// node('java'){
-//     checkout scm
-//     stage('build Docker image'){
-//         sh "pwd && ls"
-//         sh "docker build -t manar564/data-it:v${BUILD_NUMBER} ."
-//     }
-//     stage("Push Docker image"){
-//         sh "docker push manar564/data-it:v${BUILD_NUMBER}"
-//         }
-
-// }
-
 }
