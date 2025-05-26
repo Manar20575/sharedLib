c51378fbe4feb49d7ca0cb7d172feeec9c55fd7b
Manar Hamada Elsayed <74185394+Manar20575@users.noreply.github.com>
2025-05-26 17:01:23 +0300

    Task1R

--- src/org/iti/docker.groovy
+++ src/org/iti/docker.groovy
@@ -1,13 +1,13 @@
-// package org.iti;
+package org.iti;
 
-// def login(USERNAME, PASSWORD){
-//     sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-// }
+def login(USERNAME, PASSWORD){
+    sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+}
 
-// def build(IMAGE_NAME, IMAGE_TAG){
-//     sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
-// }
+def build(IMAGE_NAME, IMAGE_TAG){
+    sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
+}
 
-// def push(IMAGE_NAME, IMAGE_TAG){
-//     sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
-// }
+def push(IMAGE_NAME, IMAGE_TAG){
+    sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
+}
